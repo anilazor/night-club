a78742816d30035a26138a23650e0f70ea68de17
rosalina hundahl <rofr0001@stud.kea.dk>
2025-12-03 12:19:50 +0100

    rosa/almost-done

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -3,24 +3,38 @@
 import Image from "next/image";
 import ActiveLink from "./ActiveLink";
 import Link from "next/link";
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const links = (
+    <>
+    </>
+  )
   return (
-    <header className=" w-screen grid grid-cols-(--project-grid-cols) project-grid">
-      <div className="h-screen  md:h-20 md:flex items-center md:justify-between ">
-        <div>
-          <Link href="/">
-            <Image
-              className=""
-              src="/assets/Logo.png"
-              width={200}
-              height={200}
-              alt="logo"
-            />
-          </Link>
-        </div>
-        <nav>
-          <ul className="flex flex-col md:flex-row *:mx-4 font-medium uppercase *:tracking-[2%] *:text-nowrap md:justify-between justify-center items-center  text-center  md:text-start *:py-3 ">
+    <header className="  w-screen grid grid-cols-(--project-grid-cols) project-grid">
+      <nav>
+        <div className="h-25 flex items-center justify-between w-full">
+
+            <Link href="/">
+              <Image
+                className=""
+                src="/assets/Logo.png"
+                width={200}
+                height={200}
+                alt="logo"
+              />
+            </Link>
+
+            <button
+              className="flex-0 cursor-pointer md:hidden"
+              onClick={() => setOpen((s) => !s)}>
+              <IoMenu size={40} />
+            </button>
+          
+
+          <ul className={`${open ? "" : "opacity-0 md:opacity-100"} flex flex-col md:flex-row *:mx-4 font-medium uppercase *:tracking-[2%] *:text-nowrap  justify-center items-center  text-center  md:text-start *:py-3`}>
             <li>
               <ActiveLink href="/">home</ActiveLink>
             </li>
@@ -37,8 +51,8 @@ export default function Header() {
               <ActiveLink href="/login">login</ActiveLink>
             </li>
           </ul>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </header>
   );
 }
