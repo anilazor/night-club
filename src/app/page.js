19082ee4c1b1e9d3eedb654dfdea0ab104828a48
Celine Sophie <cevo0001@stud.kea.dk>
2025-12-06 13:59:44 +0100

    celine-welcome-komponenter

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import HomeHero from "./components/home/hero/HomeHero";
 import Events from "./components/home/events/Events";
 import Gallery from "./components/home/gallery/Gallery";
+import Welcome from "./components/home/welcome/Welcome";
+
 
 export default function Home() {
   return (
     <>
       <main className="grid col-(--full-col) grid-cols-subgrid">
         <HomeHero />
+
+        <Welcome />
+      
         <Events />
 
         <Gallery />
