d27900e9363beef4726498c4155faf1dd5dfb183
Celine Sophie <cevo0001@stud.kea.dk>
2025-12-08 13:46:21 +0100

    celine-dynamisk-api-testimonialcard

--- src/app/components/testimonials/TetimonialCard.jsx
+++ src/app/components/testimonials/TetimonialCard.jsx
@@ -2,27 +2,24 @@ import SoMe from "../socials/Socials";
 import Image from "next/image";
 import { HeadingSecondary, Caption } from "../typography";
 
-const testimonials = () => {
-  let image = "/assets/content-img/testimonial_1.jpg";
+const TestimonialCard = ({image, name, text}) => {
+ 
   return (
     <div className="text-center flex flex-col  justify-center items-center gap-5 mt-12 mb-8">
       <Image
         src={image}
         width={200}
         height={200}
-        alt="Portrait image"
+        alt={`Portait of ${name}`}
         className=""
       />
       <div className="*:py-2 mb-2 max-w-[102ch]">
-        <HeadingSecondary text="Alex" />
-        <Caption
-          text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by 
-injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn’t anything embarassing hidden in the middle of the text."
-        />
+        <HeadingSecondary text={name} />
+        <Caption text={text}/>
       </div>
       <SoMe />
     </div>
   );
 };
 
-export default testimonials;
+export default TestimonialCard;
