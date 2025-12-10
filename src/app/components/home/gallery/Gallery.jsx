2726bb52c10f11fa5ca0e9a4a93727a5f87ec6f5
Ragnhild <ragu0002@stud.kea.dk>
2025-12-10 11:35:03 +0100

    ragnhild-add-hover-animation-gallery

--- src/app/components/home/gallery/Gallery.jsx
+++ src/app/components/home/gallery/Gallery.jsx
@@ -2,6 +2,7 @@
 
 import { HeadingMain } from "../../typography";
 import { useEffect, useState } from "react";
+import Triangles from "../../hoverFrames/Triangles";
 import axios from "axios";
 import Image from "next/image";
 import PatternBg from "../../bgOverlays/PatternBg";
@@ -31,13 +32,19 @@ export default function Gallery() {
           <HeadingMain color="white" text="night club gallery" />
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-4 *:object-cover *:self-stretch ">
+        <div className="grid grid-cols-1 md:grid-cols-6 [&>*:nth-child(1)]:md:col-span-2 [&>*:nth-child(1)]:md-row-span-1 [&>*:nth-child(2)]:md:col-span-1 [&>*:nth-child(2)]:md-row-span-1  [&>*:nth-child(3)]:md:col-span-2 [&>*:nth-child(3)]:md-row-span-1 [&>*:nth-child(4)]:md:col-span-1 [&>*:nth-child(4)]:md-row-span-1 [&>*:nth-child(5)]:md:col-span-2 [&>*:nth-child(5)]:md-row-span-2 [&>*:nth-child(6)]:md:col-span-2 [&>*:nth-child(6)]:md-row-span-2 [&>*:nth-child(7)]:md:col-span-2 [&>*:nth-child(7)]:md-row-span-2 *:object-cover *:self-stretch ">
           {!isLoading &&
             !error &&
             isImage.length > 0 &&
             isImage.slice(0, 7).map((img) => {
               const filename = img.asset.url.split("/").pop();
-              return <Image key={img.id} src={`/assets/content-img/${filename}`} alt={filename || img.description || `Gallery image ${img.id}`} className="w-full h-full object-cover" width={1200} height={800} />;
+              return (
+                <Triangles key={img.id}>
+                  <div className="h-65">
+                    <Image key={img.id} src={`/assets/content-img/${filename}`} alt={filename || img.description || `Gallery image ${img.id}`} className="w-full h-full object-cover" width={1200} height={800} />;
+                  </div>
+                </Triangles>
+              );
             })}
         </div>
       </section>
